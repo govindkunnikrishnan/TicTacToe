a727876936f1dbc00a180b4b8351dbaed0acb6ce
govindunnikrishnan <govind.ku24@gmail.com>
2019-03-21 19:41:55 +0530

    Handling Clicks on Squares
    Changed Square Component to controlled components as they don't keep state
    Board Component now keep log of the state of square components

--- src/index.js
+++ src/index.js
@@ -7,16 +7,34 @@ import * as serviceWorker from './serviceWorker';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={() => this.props.squareClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        squareClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
